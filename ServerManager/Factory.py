1522f8b34b0e6dd3171391eeb9b3e2c0206e9f70
Sina <sina_khorami@comp.iust.ac.ir>
2016-01-25 11:22:17 +0330

    Server Manager

---
+++ ServerManager/Factory.py
@@ -0,0 +1,15 @@
+
+
+class Factory():
+	def __init__(self):
+		self.result = {}
+
+	def getResult(self, component, message, device_id):
+		if   component == "search":
+			pass
+		elif component == "find":
+			pass
+		else:
+			pass
+
+		return self.result
---
+++ ServerManager/Main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from Factory import Factory
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def index():
+	'''
+		Json receiving request from client has 3 key
+		[component] : destination component
+		[message]   : content of user speech changed to text
+		[device_id] : device id of client (unique)
+	'''
+	factory = Factory()
+	component = request.json["component"]
+	device_id = request.json["device_id"]
+	message   = request.json["message"]
+	result = factory.getResult(component, message, device_id)
+	return jsonify(result), 200
+
+if __name__ == '__main__':
+	app.run()
