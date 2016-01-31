6f180bd05f9598c1365290af3cbb4101fbb89e9f
Sina <sina_khorami@comp.iust.ac.ir>
2016-01-31 11:39:30 +0330

    add Interests

---
+++ Interests/interestManager.py
@@ -0,0 +1,36 @@
+from interests import *
+
+
+class InterestManager():
+    def __init__(self, data, a_id):
+        self.data = data
+        self.userInterests = Interests(a_id).getInterests()
+        self.interests = ['sports', 'news', 'travel', 'food and drinks', 'events', 'places', 
+                          'movies and tv', 'music']
+        self.data = data.lower()
+        self.spl = data.split()
+        self.new_data = ""
+        if ("search" in self.spl):
+            self.searchBasedOnInterests()
+        else:
+            print ("Not Found!")
+        
+    def searchBasedOnInterests(self):
+        for word in self.spl:
+            for interest in self.interests:
+                if word in interest:
+                    try :
+                        for i in list(self.userInterests.keys()):
+                            if (word in i):
+                                self.new_data = self.data.replace(word, self.userInterests[i][0])
+                    except KeyError:
+                        print ("OK!")
+    def locationBasedOnInterests(self):
+        pass
+        
+        
+        
+        
+#a = InterestManager("search latest drink news", "123")
+#print(a.userInterests)
+#print(a.new_data)
---
+++ Interests/interests.py
@@ -0,0 +1,74 @@
+import xml.etree.ElementTree as ET
+
+class Interests:
+    
+    def __init__(self,a_id):
+        self.id = a_id
+        self.dic = {}
+        tree = ET.parse("data.xml")
+        self.root = tree.getroot()
+        self.setUser()
+        
+    def setUser(self):
+        flag = False
+        new_user = ""
+        for user in self.root:
+            if user.attrib['id'] == self.id:
+                flag = True
+        if flag == False:
+            new_user = ET.Element('user', {'id' : self.id})
+            self.root.append(new_user)
+            tree= ET.ElementTree(self.root)
+            tree.write("data.xml")
+                
+    def setInterests(self, data):
+        data = dict((k.lower(), v) for k,v in data.iteritems())
+        flag = False
+        for user in self.root:
+            if user.attrib['id'] == self.id:
+                for type_ in user:
+                    if(type_.attrib['title'] == list(data.keys())[0]):
+                        flag = True
+                        new_child = ET.Element('value', {'value': list(data.values())[0]})
+                        type_.append(new_child)
+                        tree = ET.ElementTree(self.root)
+                        tree.write("data.xml")
+                if(flag == False):
+                    new_child = ET.Element('value', {'value': list(data.values())[0]})
+                    new_element =  ET.Element('type', {'title' : list(data.keys())[0]})
+                    new_element.append(new_child)
+                    user.append(new_element)
+                    tree = ET.ElementTree(self.root)
+                    tree.write("data.xml")
+                    break
+    def getInterests(self):
+        for user in self.root:
+            if user.attrib['id'] == self.id:
+                for type_ in user:
+                    attr = type_.attrib['title']
+                    self.dic[attr]= []
+                    for value in type_:
+                        self.dic[attr].append(value.attrib['value'])
+        return (self.dic)
+        
+    def deleteInterests(self, data):
+        data = dict((k.lower(), v) for k,v in data.iteritems())
+        flag = False
+        for user in self.root:
+            if (user.attrib['id'] == self.id) and flag == False:
+                for type_ in user:
+                    if (type_.attrib['title'] == list(data.keys())[0]) and flag == False:
+                        for value in type_:
+                            if (value.attrib['value'] == list(data.values())[0]) and flag == False:
+                                type_.remove(value)
+                                flag = True
+                        if len(type_) == 0:
+                            user.remove(type_)
+                        tree = ET.ElementTree(self.root)
+                        tree.write("data.xml")
+
+
+#user = Interests("1247")
+#user.getInterests()
+#data = {"Movie" : "A Seperation"}
+#user.deleteInterests(data)
